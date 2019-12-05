ff690b181845b70c2e979538c3a3f6ccd8f1a41c
AishwaryaPedemane <56244695+AishwaryaPedemane@users.noreply.github.com>
2019-12-05 17:21:38 +0530

    Update start.sh

--- script/start.sh
+++ script/start.sh
@@ -3,4 +3,4 @@
 
 # The nohup utility makes the command passed as an argument run in the background even after you log out.
 # The & symbol, switches the program to run in the background.
-nohup java -jar ../bin/A365AuthIntegration-0.0.1-SNAPSHOT.jar --spring.config.location=../config/application.yml > /dev/null &
+nohup java -jar ../bin/docker-spring-data-rest.jar > /dev/null &
